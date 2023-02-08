826d66cbee3d4340449f12890f27bec04e92ba8a
agusutomo <101611153+agusutomo@users.noreply.github.com>
2023-02-08 08:28:05 +0700

    Update ssi-test.php

--- ssi-test.php
+++ ssi-test.php
@@ -1,16 +1,4 @@
 <?php
-echo "
-<!DOCTYPE html>
-<html>
-<head>
-<title>SHTML with include example</title>
-</head>
-<body>
-
-<h1>My First Heading</h1>
-<!--#include virtual="header.php" -->
-<p>The above contents are included from the header.php (HTML code) file</p>
-
-</body>
-</html>";
+echo "<h3>Hello World, let's test our Kinsta Cloud (PHP) App!</h3>";
+echo '<!--#include virtual="header.php" -->';
 ?>
