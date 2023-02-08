4878cb1e2757c7ddad6e309f583a62fd21a68e43
agusutomo <101611153+agusutomo@users.noreply.github.com>
2023-02-08 08:22:51 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,13 @@
+<?php 
+
+echo"
+<!DOCTYPE html>
+<html>
+<head>
+<title>It's the content of header.php (HTML codes)</title>
+</head>
+<body>
+<h1>It's the content of header.php (HTML codes)</h1>
+</body>
+</html>";
+?>
