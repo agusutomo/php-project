f4e2b85836c4ff98e10c574b62c396fe419f8749
agusutomo <101611153+agusutomo@users.noreply.github.com>
2023-02-08 08:24:58 +0700

    Create ssi-test.php

---
+++ ssi-test.php
@@ -0,0 +1,16 @@
+<?php
+echo "
+<!DOCTYPE html>
+<html>
+<head>
+<title>SHTML with include example</title>
+</head>
+<body>
+
+<h1>My First Heading</h1>
+<!--#include virtual="header.php" -->
+<p>The above contents are included from the header.php (HTML code) file</p>
+
+</body>
+</html>";
+?>
