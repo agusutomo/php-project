71a72b622231d494c0e264063e0d4516907a19be
agusutomo <101611153+agusutomo@users.noreply.github.com>
2023-07-28 09:13:56 +0700

    Update gif.php

--- gif.php
+++ gif.php
@@ -1,3 +1,11 @@
 <?php
- echo '<img src="../datadisk/you-got-it-success.gif" />';
+   
+  // Specifying directory
+  $mydir = '/datadisk';
+ 
+  // Scanning files in a given directory in ascending order
+  $myfiles = scandir($mydir);
+ 
+  // Displaying the files in the directory
+  print_r($myfiles);
 ?>
